bbc001a35833bb405782bbe7720c76944dbb09b4
PavelEremeev <pe19950520@yandex.ru>
2021-02-27 20:14:46 +0300

    fixing newsApi url

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,5 +1,5 @@
-import moment from 'moment' 
-import {SEARCHDAYS} from './constants'
+import moment from 'moment'
+import { SEARCHDAYS } from './constants'
 const newsApiKey = 'fd1e859e79f74af4b07453725f46383e';
 const today = moment().format()
 const weekAgo = moment().subtract(SEARCHDAYS, 'days').format()
@@ -16,7 +16,7 @@ class NewsApi {
     }
 
     getNewsCardList(searchWord) {
-        return fetch(`${this._address}/v2/everything?q=${searchWord}&from=${this._from}&to=${this._to}&pageSize=${this._pageSize}`, {
+        return fetch(`${this._address}/news/v2/everything?q=${searchWord}&from=${this._from}&to=${this._to}&pageSize=${this._pageSize}`, {
             headers: {
                 authorization: newsApiKey,
             },
@@ -24,11 +24,21 @@ class NewsApi {
             res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
         );
     }
+
+    // getNewsCardList(searchWord) {
+    //     return fetch(`${this._address}/v2/everything?q=${searchWord}&from=${this._from}&to=${this._to}&pageSize=${this._pageSize}`, {
+    //         headers: {
+    //             authorization: newsApiKey,
+    //         },
+    //     }).then((res) =>
+    //         res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+    //     );
+    // }
 }
 
 export const newsApi = new NewsApi({
-    address: 'https://newsapi.org',
-    pageSize: 100, 
+    address: 'https://nomoreparties.co',
+    pageSize: 100,
     from: weekAgo,
     to: today,
 });
